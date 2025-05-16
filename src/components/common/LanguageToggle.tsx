07063067b28b4e0124b02a9d5a0835bedd891582
linnlatt132 <clinnlatt@gmail.com>
2025-05-16 16:35:33 +0630

    Login page and image slider

--- src/components/common/LanguageToggle.tsx
+++ src/components/common/LanguageToggle.tsx
@@ -1,16 +1,53 @@
 import i18next from 'i18next'
 import Button from './Button'
+import { useState } from 'react'
+import { Globe, ChevronDown, ChevronUp } from 'lucide-react'
+
 const LanguageToggle = () => {
-  const changeLanguage = (lng: string) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [currentLang, setCurrentLang] = useState<'en' | 'mm'>(
+    i18next.language as 'en' | 'mm',
+  )
+  const changeLanguage = (lng: 'en' | 'mm') => {
     i18next.changeLanguage(lng)
+    setCurrentLang(lng)
+    setIsOpen(false)
   }
+
   return (
-    <div>
-      <div>
+    <div className='relative inline-block text-left'>
+      {/* <div>
         <Button primary onClick={() => changeLanguage('mm')}>mm</Button>
         <Button secondary onClick={() => changeLanguage('en')}>en</Button>
         
-      </div>
+      </div> */}
+      <Button
+        outline
+        onClick={() => setIsOpen(!isOpen)}
+        className='flex items-center gap-1 rounded-l-full rounded-r-full px-2 text-[14px] py-1 '
+      >
+        <Globe size={25} className='pr-1' />
+        <span className='capitalize'>
+          {currentLang === 'en' ? 'English' : 'Myanmar'}
+        </span>
+        {isOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
+      </Button>
+      {isOpen && (
+        <div className='border-secondary text-secondary absolute z-10 m-1 rounded-lg border bg-white px-2 shadow-lg'>
+          <button
+            className='block w-full px-5 py-2 text-left hover:cursor-pointer hover:underline'
+            onClick={() => changeLanguage('en')}
+          >
+            English
+          </button>
+          <button
+            className='block w-full px-5 py-2 text-left hover:cursor-pointer hover:underline'
+            onClick={() => changeLanguage('mm')}
+          >
+            Myanmar
+          </button>
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,19 +1,53 @@
-import { AppConstantRoutes } from '@/services/routes/path'
-import { useNavigate } from 'react-router'
+import Button from '@/components/common/Button'
+import Input from '@/components/common/Input'
+import LanguageToggle from '@/components/common/LanguageToggle'
+import ImageSlider from '@/components/imageSlider'
+import { useTranslation } from 'react-i18next'
 
 const Login = () => {
-  const navigate = useNavigate()
+  const { t } = useTranslation()
   return (
-    <div className='flex h-screen flex-col items-center justify-center bg-gray-100'>
-      <div className='w-96 rounded bg-white p-8 shadow-md'>
-        <h2 className='mb-6 text-center text-2xl font-bold'>Login</h2>
+    <div className='m-5 flex min-h-screen items-center justify-center'>
+      <div className='flex w-[1000px] max-w-5xl overflow-hidden shadow-lg'>
+        {/* Slider */}
+        <div className='w-1/2'>
+          <ImageSlider />
+        </div>
+        {/* Login form */}
+        <div className='relative flex w-1/2 flex-col items-center justify-center rounded-r-[10px] border border-l-0 border-[#333334]/30 bg-[#F5FEFD] p-9'>
+          <div className='absolute top-4 right-5'>
+            <LanguageToggle />
+          </div>
+          <form className='items-center justify-center space-y-5'>
+            <div>
+              <h1 className='text-[32px] font-medium text-[#333334]'>
+                {t('Login.welcome')}
+              </h1>
+              <h3 className='text-[16px] font-light text-[#333334]/50'>
+                {t('Login.instruction')}
+              </h3>
+            </div>
+            <Input placeholder={t('Login.email')} />
+            <Input placeholder={t('Login.password')} type='password' />
 
-        <button
-          onClick={() => navigate(AppConstantRoutes.paths.example.default)}
-          className='w-[200px] rounded bg-green-300 py-2 text-center'
-        >
-          Go to Example
-        </button>
+            <div className='flex justify-between'>
+              <Button primary type='submit' className='w-30'>
+                {t('Login.login')}
+              </Button>
+              <a
+                href='#'
+                className='mt-2 text-sm font-light text-[#333334]/50 underline hover:italic'
+              >
+                {t('Login.forgot')}
+              </a>
+            </div>
+            <div className='mt-5 border-t border-[#333334]/30 pt-8 text-center'>
+              <Button outline className='w-full'>
+                {t('Login.create')}
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
