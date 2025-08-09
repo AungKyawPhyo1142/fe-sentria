3e578075bd9eba02f89696b2e713eb578bf0fb19
SawZiDunn <szidunn199@gmail.com>
2025-08-09 20:21:48 +0700

    update: update modal styles

---
+++ src/components/posts/constants/constants.ts
@@ -0,0 +1,28 @@
+export const backdropVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.3 } },
+  exit: { opacity: 0, transition: { duration: 0.2 } },
+}
+
+export const modalVariants = {
+  hidden: { opacity: 0, scale: 0.8, y: -50 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      type: 'spring' as const,
+      stiffness: 200,
+      damping: 25,
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.85,
+    y: 40,
+    transition: {
+      duration: 0.2,
+      ease: 'easeInOut' as const,
+    },
+  },
+}
