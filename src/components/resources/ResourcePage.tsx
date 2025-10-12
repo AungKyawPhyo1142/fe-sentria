dc1373b04597c21f795a835eb3c7012dad794e65
linnlatt132 <clinnlatt@gmail.com>
2025-10-12 20:36:22 +0630

    UI fix with resource create button when the user is only verified

--- src/components/resources/ResourcePage.tsx
+++ src/components/resources/ResourcePage.tsx
@@ -11,6 +11,7 @@ import {
 import {
   UserProfileMap,
   useBatchUserProfiles,
+  useUserProfile,
 } from '@/services/network/lib/user'
 import { selectAuth, useAuthStore } from '@/zustand/authStore'
 import {
@@ -31,6 +32,9 @@ export default function ResourcePage() {
   const [sortOrder, setSortOrder] = useState<'latest' | 'oldest'>('latest')
 
   const { userId: currentUserId } = useAuthStore(selectAuth)
+  const { data: userProfile } = useUserProfile(String(currentUserId))
+
+  const isVerified = userProfile?.verified_profile
 
   const { data: resourcesData, isLoading: resourcesLoading } = useGetResources()
 
@@ -201,13 +205,19 @@ export default function ResourcePage() {
               </p>
             )}
           </div>
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className='bg-primary flex h-12.5 flex-shrink-0 items-center justify-center rounded-xl px-4 py-1 font-light text-white hover:cursor-pointer'
-          >
-            <CirclePlus size={26} strokeWidth={1} />
-            <span className='ml-3 text-[16px]'>Create a resource</span>
-          </button>
+          {isVerified ? (
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className='bg-primary flex h-12.5 flex-shrink-0 items-center justify-center rounded-xl px-4 py-1 font-light text-white hover:cursor-pointer'
+            >
+              <CirclePlus size={26} strokeWidth={1} />
+              <span className='ml-3 text-[16px]'>Create a resource</span>
+            </button>
+          ) : (
+            <div className='min-h-[50px] items-center justify-center text-center flex rounded-lg border border-red-300 bg-red-50/50 px-4  text-xs text-red-700'>
+              Verify your profile to create resources
+            </div>
+          )}
         </div>
 
         {/* Resource List */}
